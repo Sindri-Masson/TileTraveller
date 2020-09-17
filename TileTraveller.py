71f80f7bc9b101e763fbd54004d048ad1c1aecbc
Sindri <sindrim19@ru.is>
2020-09-17 14:45:57 +0000

    Algorithm

---
+++ TileTraveller.py
@@ -0,0 +1,7 @@
+# Forritið byrjar á reitnum (1, 1) þ.e.a.s. x = 1 og y = 1 og if setningar tékka á því hvert spilari getur fært sig
+# Ef x == 1 og y == 1 getur spilari farið í þessar áttir. Þegar búið er að tékka hvert hægt er að færa sig 
+# Kemur input þar sem  spilari er beðinn um átt sem hann vill færa sig og if setning testar hvort hann hafi slegið
+# inn valid átt. Þegar það er búið er spilari færður í þá átt sem hann vildi fara. While setning er sett utanum 
+# sem runnar ef reiturinn sem spilari er á er einhver annar en (3, 1) og í endann er print skipun sem prentar 
+# "Victory"
+
